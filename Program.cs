a90fc86d49612622ac62bbcfe1920d1dbdae47d7
sippo <sippo@DESKTOP-4013P7G>
2024-01-18 15:02:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Practice1");
+        }
+    }
+}
